ecb3a757df90557719a77e086f7ac68653a2a287
kosmos <119829358+kosmoslol@users.noreply.github.com>
2025-09-24 17:39:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from "@tomphttp/bare-server-node";
+import path from 'node:path';
+import cors from 'cors';
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer('/outerspace/');
+const PORT = 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
 
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
+
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/ypxa/y/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
 });
 
-function isUrl(val = ''){
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
+});
+
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
+
+server.listen({
+  port: PORT,
+});
